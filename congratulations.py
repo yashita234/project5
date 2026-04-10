09da7b1600b39b09d7f9629e4f358e1f27c12eb7
yashita234 <kataruyashitareddy2605@gmail.com>
2026-04-10 09:54:01 +0530

    Add files via upload

---
+++ congratulations.py
@@ -0,0 +1,22 @@
+name = input("Enter your name: ")       
+age = int(input("Enter your age: "))    
+score = float(input("Enter your score: "))  
+is_winner = True                       
+
+
+print("\n Congratulations Message ")
+
+print("Hello", name + "!")
+print("You are", age, "years old.")
+print("You scored", score, "marks.")
+
+print("Winner Status:", is_winner)
+print("Data Types Used:")
+print("Name:", type(name))
+print("Age:", type(age))
+print("Score:", type(score))
+print("Winner:", type(is_winner))
+
+
+print("\n Congratulations", name + "!")
+print("Keep achieving great things ")
