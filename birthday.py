508a13911eb7164961201618ce428cf827ae5ac7
yashita234 <kataruyashitareddy2605@gmail.com>
2026-04-12 11:37:36 +0530

    Add files via upload

---
+++ birthday.py
@@ -0,0 +1,11 @@
+bday1 = "12-01-2008"
+bday2 = "25-03-2007"
+bday3 = "07-07-2008"
+bday4 = "19-10-2007"
+bday5 = "02-12-2008"
+
+print("Birthday 1:", bday1)
+print("Birthday 2:", bday2)
+print("Birthday 3:", bday3)
+print("Birthday 4:", bday4)
+print("Birthday 5:", bday5)
